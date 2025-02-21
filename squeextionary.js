57c378843307e31b353c7b5dd2cb4e572e1c48df
driedapps <malmcbb@gmail.com>
2025-02-21 09:09:17 -0500

    Add files via upload

---
+++ squeextionary.js
@@ -0,0 +1,161 @@
+export const vods = [
+    {
+        filepath: 'screenshots/shot2.png',
+        voddate: '10-02-2024',
+        game: 'UFO 50',
+        vodlink: 'https://www.twitch.tv/videos/2266307516'
+    },
+
+    {
+        filepath: 'screenshots/shot3.png',
+        voddate: '09-06-2024',
+        game: 'Super Mario 64',
+        vodlink: 'https://www.twitch.tv/videos/2243958523'
+    },
+
+    {
+        filepath: 'screenshots/shot4.png',
+        voddate: '11-05-2024',
+        game: 'Rocket League',
+        vodlink: 'https://www.twitch.tv/videos/2294504340'
+    },
+
+    {
+        filepath: 'screenshots/shot5.png',
+        voddate: '03-11-2022',
+        game: 'Elden Ring',
+        vodlink: 'https://www.youtube.com/watch?v=hmpZNzvhW5Y'
+    },
+    {
+        filepath: 'screenshots/shot6.png',
+        voddate: '04-03-2022',
+        game: 'Kirby and the Forgotten Land',
+        vodlink: 'https://www.youtube.com/watch?v=Is5eh4ov9no'
+    },
+    {
+        filepath: 'screenshots/shot7.png',
+        voddate: '03-21-2022',
+        game: 'Elden Ring',
+        vodlink: 'https://www.youtube.com/watch?v=ur_hsLPbxaQ'
+    },
+    {
+        filepath: 'screenshots/shot8.png',
+        voddate: '04-06-2022',
+        game: 'God of War',
+        vodlink: 'https://www.youtube.com/watch?v=QKwsUzJv1Io'
+    },
+    {
+        filepath: 'screenshots/shot9.png',
+        voddate: '04-13-2022',
+        game: 'LEGO Star Wars: The Skywalker Saga Part 4',
+        vodlink: 'https://www.youtube.com/watch?v=_z-AbveKlxg'
+    },
+    {
+        filepath: 'screenshots/shot10.png',
+        voddate: '04-17-2022',
+        game: 'Super Mario 64',
+        vodlink: 'https://www.youtube.com/watch?v=7Mate5LNz4A'
+    },
+    {
+        filepath: 'screenshots/shot11.png',
+        voddate: '04-17-2022',
+        game: 'At Dead of Night',
+        vodlink: 'https://www.youtube.com/watch?v=635uU4qNQ-4'
+    },
+    {
+        filepath: 'screenshots/shot12.png',
+        voddate: '04-18-2022',
+        game: 'Super Mario 64',
+        vodlink: 'https://www.youtube.com/watch?v=e_pAiqb5fyc'
+    },
+    {
+        filepath: 'screenshots/shot13.png',
+        voddate: '04-24-2022',
+        game: 'Super Mario 64',
+        vodlink: 'https://www.youtube.com/watch?v=WNAEracD7BM'
+    },
+    {
+        filepath: 'screenshots/shot14.png',
+        voddate: '02-11-2025',
+        game: 'The Long Drive',
+        vodlink: 'https://www.youtube.com/watch?v=-Cu59RgsghQ'
+    },
+    {
+        filepath: 'screenshots/shot15.png',
+        voddate: '04-27-2022',
+        game: 'Fortnite',
+        vodlink: 'https://www.youtube.com/watch?v=0qrXm2QKXhY'
+    },
+    {
+        filepath: 'screenshots/shot16.png',
+        voddate: '05-01-2022',
+        game: 'The Stanley Parable: Ultra Deluxe',
+        vodlink: 'https://www.youtube.com/watch?v=xwpzN3xjCXw'
+    },
+    {
+        filepath: 'screenshots/shot17.png',
+        voddate: '05-04-2022',
+        game: 'Valorant',
+        vodlink: 'https://www.youtube.com/watch?v=7L_FuoaH4c0'
+    },
+    {
+        filepath: 'screenshots/shot18.png',
+        voddate: '05-13-2022',
+        game: 'Super Mario Sunshine',
+        vodlink: 'https://www.youtube.com/watch?v=zG1VgaRtFGI'
+    },
+    {
+        filepath: 'screenshots/shot19.png',
+        voddate: '05-17-2022',
+        game: 'The Sims 4',
+        vodlink: 'https://www.youtube.com/watch?v=LEexMWaR7ZA'
+    },
+    {
+        filepath: 'screenshots/shot20.png',
+        voddate: '03-03-2016',
+        game: 'Counter-Strike',
+        vodlink: 'https://www.twitch.tv/videos/52177290'
+    },
+    {
+        filepath: 'screenshots/shot21.png',
+        voddate: '05-28-2014',
+        game: 'Counter-Strike',
+        vodlink: 'https://www.twitch.tv/videos/49148191'
+    },
+    {
+        filepath: 'screenshots/shot22.png',
+        voddate: '01-17-2019',
+        game: 'DARK SOULS 3',
+        vodlink: 'https://www.twitch.tv/videos/365345182'
+    },
+    {
+        filepath: 'screenshots/shot23.png',
+        voddate: '05-17-2022',
+        game: 'Luigis Mansion',
+        vodlink: 'https://www.youtube.com/watch?v=LkiuDmbzQ6I'
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+]
